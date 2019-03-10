17366db4ec582ead8c6c9290c7be3d90ba9c0de4
Vitalii Manchenko <vitalii.manchenko@rolique.io>
2019-03-11 00:22:20 +0200

    feature: Added containers and switch button.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ getJson('chart_data.json').then(start);
 
 function start(chartsData) {
   var rootContainer = document.getElementById('root');
+  var chartContainer = document.getElementById('chart');
+  var switchButton = document.getElementById('switch-button');
 
   console.log(chartsData);
 
-
+  console.log(
+    rootContainer,
+    chartContainer,
+    switchButton,
+  );
 }
